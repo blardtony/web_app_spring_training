33c0c9037c1a7e18db87c0aba5c7cb4449e2f610
Tony Blard <tony.blard@epita.fr>
2022-10-26 12:04:42 +0200

    Add Rest Controller annotation

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,8 +2,10 @@ package fr.lernejo.todo;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 public class TodoListController {
 
     @GetMapping("/api/todo")
