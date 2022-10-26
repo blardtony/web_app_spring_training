18773086a03f81843498e8834b37f2422e9b9d5b
Tony Blard <tony.blard@epita.fr>
2022-10-26 12:39:23 +0200

    Reafactor TodoLis tApplication and Controller

--- src/main/java/fr/lernejo/todo/TodoListApplication.java
+++ src/main/java/fr/lernejo/todo/TodoListApplication.java
@@ -1,7 +1,5 @@
 package fr.lernejo.todo;
 
-import javax.swing.Spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -5,20 +5,22 @@ import java.util.Collection;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequestMapping("api/todo")
 public class TodoListController {
 
     private final List<Todo> todoRepo = new ArrayList<>();
 
-    @GetMapping("/api/todo")
+    @GetMapping
     public Collection <Todo> getTodo() {
         return todoRepo;
     }
 
-    @PostMapping("/api/todo")
+    @PostMapping
     public void addTodo(@RequestBody Todo todo) {
         todoRepo.add(todo);
     }
